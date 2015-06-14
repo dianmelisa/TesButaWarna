9811336bb11c660d46fde6815def8d2540f474fe
Dian Melisa M <dian.melisa@elektro-unsyiah.net>
2015-06-14 14:42:22 +0700

    Penambahan Soal , HasilActivity sudah diperbaiki

---
+++ app/src/main/java/id/ac/unsyiah/elektro/mobile/tesbutawarna/Soal7.java
@@ -0,0 +1,60 @@
+package id.ac.unsyiah.elektro.mobile.tesbutawarna;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class Soal7 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal7);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_soal7, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickBtnNext7(View v) {
+        EditText periksaAnsTxt = (EditText) findViewById(R.id.et);
+
+        int periksaAns = Integer.valueOf(periksaAnsTxt.getText().toString());
+        Intent hasilIntent = new Intent(this, Soal8.class);
+        int pointHasil;
+
+        if (periksaAns == 3){
+            pointHasil = 1;
+            hasilIntent.putExtra("pointHasil7",pointHasil);
+
+        }else {
+            pointHasil = 0;
+            hasilIntent.putExtra("pointHasil7", pointHasil);
+        }
+        startActivity(hasilIntent);
+    }
+}
